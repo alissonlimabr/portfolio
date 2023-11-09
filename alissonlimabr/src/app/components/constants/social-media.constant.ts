3a110e803bd2cfcc3ec32e4f12de75c18ce3debc
Alisson Lima <alissonlimabr.github@gmail.com>
2023-11-09 05:49:51 -0500

    feat: add section de contatos

---
+++ alissonlimabr/src/app/components/constants/social-media.constant.ts
@@ -0,0 +1,37 @@
+export const SOCIAL_MEDIA = [
+  {
+    name: 'Gmail',
+    icon: 'assets/icons/gmail-icon.svg',
+    url: 'mailto:alissonlimabr.dev@gmail.com',
+    alt: 'Acesse o gmail de alissonlimabr.dev@gmail.com',
+    aosAnimation: 'zoom-out-down',
+  },
+  {
+    name: 'Instagram',
+    icon: 'assets/icons/instagram-icon.svg',
+    url: 'https://www.instagram.com/alisson_ml/',
+    alt: 'Acesse o instagram de alisson_ml',
+    aosAnimation: 'zoom-out-up',
+  },
+  {
+    name: 'LinkedIn',
+    icon: 'assets/icons/linkedin-icon.svg',
+    url: 'https://www.linkedin.com/in/alissonlimadev',
+    alt: 'Acesse o instagram de alisson_ml',
+    aosAnimation: 'zoom-out-down',
+  },
+  {
+    name: 'GitHub',
+    icon: 'assets/icons/github-icon.svg',
+    url: 'https://github.com/alissonlimabr',
+    alt: 'Acesse o github de alissonlimabr',
+    aosAnimation: 'zoom-out-up',
+  },
+  {
+    name: 'Discord',
+    icon: 'assets/icons/discord-icon.svg',
+    url: 'https://discord.com/users/477626869211201537',
+    alt: 'Acesse o discord de Alisson Lima',
+    aosAnimation: 'zoom-out-down',
+  },
+];
