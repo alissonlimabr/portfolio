d115adf29d779121008ce800835d00466ac7a6d4
Alisson Lima <alissonlimabr.github@gmail.com>
2023-11-09 22:05:23 -0500

    feat: add skills da plataforma de eventos

---
+++ alissonlimabr/src/app/components/constants/event-plataform-skills.constant.ts
@@ -0,0 +1,22 @@
+export const EVENT_PLATAFORM_SKILLS = [
+  {
+    icon: 'assets/icons/reactjs-icon.svg',
+    alt: 'Logo do Reacjs',
+  },
+  {
+    icon: 'assets/icons/graphql-icon.svg',
+    alt: 'Logo do GraphQL',
+  },
+  {
+    icon: 'assets/icons/typescript-icon.svg',
+    alt: 'Logo do Typescript',
+  },
+  {
+    icon: 'assets/icons/tailwind-icon.svg',
+    alt: 'Logo do Tailwind',
+  },
+  {
+    icon: 'assets/icons/git-icon.svg',
+    alt: 'Logo do Git',
+  },
+];
