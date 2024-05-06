b6b87325703a2e8ff90671fd1291f0168af17ca7
Alisson Lima <alissonlimabr.github@gmail.com>
2024-05-06 17:05:36 -0500

    feat: add constant com a lista de jobs

---
+++ alissonlimabr/src/app/constants/jobs.constant.ts
@@ -0,0 +1,43 @@
+export const JOBS = [
+  {
+    company: 'Freelancer',
+    position: 'Desenvolvedor Full Stack',
+    description: [
+      'Desenvolvimento de projetos Freelancer e refinamento de portfólio;',
+      'Projeto Autenticação JWT com Spring Boot, Angular, Azure/Kubernetes e Docker.',
+      'Projeto Reset Password via e-mail desenvolvido em Angular, Java/Spring boot e AWS Cloud.',
+      'Portfólio Dev desenvolvido em Angular e Material com SSR.',
+    ],
+    duration: 'Janeiro 2024 - Atualmente',
+    icon: '../../../assets/icons/freelancer-icon.svg',
+  },
+  {
+    company: 'Contax S.A',
+    position: 'Desenvolvedor Full Stack Jr.',
+    description: [
+      'Desenvolvimento, manutenção e migração de sistemas da CEF (Caixa Econômica Federal) por prazo determinado.',
+      'Projetos entregues dentro do prazo e conforme os requisitos solicitados.',
+      'Principais tecnologias utilizadas: Azure DevOps, Docker, Java, Junit 5, SQL Server, PHP e Git.',
+      'Time 100% ágil: Scrum/Kanban e XP.',
+    ],
+    duration: 'Setembro 2023 - Novembro 2023',
+    icon: '../../../assets/icons/contax-icon.svg',
+  },
+  {
+    company: 'UFAC',
+    position: 'Desenvolvedor Full Stack Trainee',
+    description: [
+      'Desenvolvimento de sistemas para as empresas Motorola e Instituto Eldorado por meio do projeto WebAcademy.',
+
+      'Desenvolvi o front-end dos sistemas demandados pelos parceiros do projeto.',
+
+      'Atuei na correções de bugs/refatoração nas API’s e integrações com o Front-End',
+
+      'Principais tecnologias utilizadas: Java, Angular, Spring Boot, Typescript, Nodejs, CI/CD, Cypress, Jest, AWS Cloud, Junit5.',
+
+      'Time 100% ágil: Scrum/Kanban e XP.',
+    ],
+    duration: 'Março 2022 - Outubro 2022',
+    icon: '../../../assets/icons/ufac-icon.svg',
+  },
+];
