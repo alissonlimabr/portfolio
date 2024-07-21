dc7ced5219020074875623b4ade8cf7e68d0d9f9
Alisson Lima <alissonlimabr.github@gmail.com>
2024-07-21 16:53:40 -0500

    feat: adiciona integração com Google Analytics.

--- alissonlimabr/src/app/app.component.ts
+++ alissonlimabr/src/app/app.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { faBars, faCode, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'alissonlimabr';
   faBars = faBars;
   faXmark = faXmark;
   faCode = faCode;
   opened?: boolean;
+
+  constructor(
+    private gtmService: GoogleTagManagerService,
+    private router: Router
+  ) {
+    gtmService.addGtmToDom();
+  }
+
+  ngOnInit(): void {
+    this.router.events.forEach((item) => {
+      if (item instanceof NavigationEnd) {
+        const gtmTag = {
+          event: 'page',
+          pageName: item.url,
+        };
+
+        this.gtmService.pushTag(gtmTag);
+      }
+    });
+  }
 
   closeSideNav() {
     this.opened = false;
