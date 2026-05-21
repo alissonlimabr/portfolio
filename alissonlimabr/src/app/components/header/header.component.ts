858952352b07987e0a7d8d6784172c88a350c424
Alisson Lima <alissonlimabr.github@gmail.com>
2026-05-20 23:27:56 -0500

    feat: active dinâmico nos nav-links conforme scroll

--- alissonlimabr/src/app/components/header/header.component.ts
+++ alissonlimabr/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, EventEmitter, HostListener, Inject, Input, OnChanges, OnInit, Output, PLATFORM_ID, SimpleChanges } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbar } from '@angular/material/toolbar';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -16,15 +17,92 @@ import { faBars, faCode, faXmark } from '@fortawesome/free-solid-svg-icons';
   ],
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnChanges {
   @Output() openSidenav = new EventEmitter();
+  @Input() isBlogRoute = false;
 
   faCode = faCode;
   faBars = faBars;
   faXmark = faXmark;
   opened?: boolean;
+  activePortfolioHash = '';
+  private readonly portfolioSectionIds = ['perfil', 'sobre', 'habilidades', 'trabalhos', 'projetos', 'contato'];
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    this.syncActivePortfolioLink();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['isBlogRoute']) {
+      this.syncActivePortfolioLink();
+    }
+  }
+
+  @HostListener('window:hashchange')
+  onHashChange(): void {
+    this.syncActivePortfolioLink();
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    this.updateActivePortfolioSectionByScroll();
+  }
+
+  @HostListener('window:resize')
+  onWindowResize(): void {
+    this.updateActivePortfolioSectionByScroll();
+  }
+
+  isPortfolioLinkActive(fragment: string): boolean {
+    const hash = this.activePortfolioHash || '#perfil';
+    return hash === `#${fragment}`;
+  }
 
   closeSideNav() {
     this.opened = false;
+  }
+
+  private syncActivePortfolioLink(): void {
+    if (!isPlatformBrowser(this.platformId)) {
+      return;
+    }
+
+    if (this.isBlogRoute) {
+      return;
+    }
+
+    if (window.location.hash) {
+      this.activePortfolioHash = window.location.hash;
+      return;
+    }
+
+    this.updateActivePortfolioSectionByScroll();
+  }
+
+  private updateActivePortfolioSectionByScroll(): void {
+    if (!isPlatformBrowser(this.platformId) || this.isBlogRoute) {
+      return;
+    }
+
+    const sections = this.portfolioSectionIds
+      .map((id) => document.getElementById(id))
+      .filter((section): section is HTMLElement => section !== null);
+
+    if (!sections.length) {
+      return;
+    }
+
+    const scrollPosition = window.scrollY + 160;
+    let activeId = sections[0].id;
+
+    for (const section of sections) {
+      if (section.offsetTop <= scrollPosition) {
+        activeId = section.id;
+      }
+    }
+
+    this.activePortfolioHash = `#${activeId}`;
   }
 }
