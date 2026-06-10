9a131ac8f8dab4e1e73abf1195ebcc40e804aeab
Alisson Lima <alissonlimabr.github@gmail.com>
2026-06-10 13:25:58 -0500

    refactor: ajusta formatação do código

--- alissonlimabr/src/main.ts
+++ alissonlimabr/src/main.ts
@@ -2,7 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app/app-routing.module';
-import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 
 // Bootstrap standalone
@@ -10,7 +14,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserAnimationsModule),
     importProvidersFrom(AppRoutingModule),
-    provideHttpClient(withFetch(), withInterceptorsFromDi())
-  ]
-})
-  .catch(err => console.error(err));
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+  ],
+}).catch((err) => console.error(err));
